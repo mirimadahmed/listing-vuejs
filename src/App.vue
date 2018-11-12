4c139ac92a691f82b0af39bd0d9c54381408a733
mirimadahmed <mirimadahmed@outlook.com>
2018-11-12 16:30:21 +0500

    base 3 col layout to display cards from api. moved list to cards.vue

--- src/App.vue
+++ src/App.vue
@@ -26,34 +26,11 @@
 </template>
 
 <script>
-import axios from 'axios';
 export default {
   name: "app",
   data() {
     return {
-      listings: []
     };
-  },
-  mounted() {
-    this.loadListings();
-  },
-  methods: {
-    loadListings(){
-      var self = this;
-      var app_id = "app838WoUK7gksAto";
-      var app_key = "key4hPsF3lTzceL6g";
-      axios.get(
-          "https://api.airtable.com/v0/"+app_id+"/Weekly%20Report?maxRecords=20&view=Main%20View",
-          {
-              headers: { Authorization: "Bearer "+app_key }
-          }
-      ).then(function(response){
-        console.log(response.data.records);
-        self.listings = response.data.records;
-      }).catch(function(error){
-        console.log(error)
-      });
-    }
   }
 };
 </script>
--- src/Cards.vue
+++ src/Cards.vue
@@ -1,20 +1,42 @@
 <template>
   <div>
-      Cards
+      <b-container>
+        <b-row>
+            <b-col sm="4" v-for="listing in listings" :key="listing.id">{{listing.fields['Title/Topic']}}</b-col>
+        </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: "Cards",
   data() {
     return {
+      listings: []
     };
   },
   mounted() {
-    
+    this.loadListings();
   },
   methods: {
+    loadListings(){
+      var self = this;
+      var app_id = "app838WoUK7gksAto";
+      var app_key = "key4hPsF3lTzceL6g";
+      axios.get(
+          "https://api.airtable.com/v0/"+app_id+"/Weekly%20Report?maxRecords=20&view=Main%20View",
+          {
+              headers: { Authorization: "Bearer "+app_key }
+          }
+      ).then(function(response){
+        console.log(response.data.records);
+        self.listings = response.data.records;
+      }).catch(function(error){
+        console.log(error)
+      });
+    }
   }
 };
 </script>
