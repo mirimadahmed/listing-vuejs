5f707c07a9f95bffc0c1f80b371c746d84d16382
mirimadahmed <mirimadahmed@outlook.com>
2018-11-13 15:20:52 +0500

    removed from navbar to move search and filters to separate component

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,6 @@
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
         <router-link to="/"><b-navbar-brand>Listing Website</b-navbar-brand></router-link>
-
-        <b-collapse is-nav id="nav_collapse">
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto" v-if="this.$route.path === '/'">
-
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" v-model="searchTerm"/>
-            </b-nav-form>
-
-          </b-navbar-nav>
-
-        </b-collapse>
       </b-navbar>
       <br><br>
       <router-view ></router-view>
@@ -30,13 +17,7 @@ export default {
   name: "app",
   data() {
     return {
-      searchTerm: ''
     };
-  },
-  watch() {
-    searchTerm:{
-      this.$emit('search', this.searchTerm);
-    }
   }
 };
 </script>
--- src/Cards.vue
+++ src/Cards.vue
@@ -26,8 +26,7 @@ export default {
   name: "Cards",
   data() {
     return {
-      listings: [],
-      searchText: ''
+      listings: []
     };
   },
   mounted() {
